0070d09f7dd8a2cc413c6eb821ab9180058248fb
2300030356 <2300030356@kluniversity.in>
2025-09-25 11:50:39 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";   // ✅ use HashRouter instead
 import RecipeState from "./context/State.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RecipeState>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </RecipeState>
 );
